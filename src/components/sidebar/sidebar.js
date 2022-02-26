3e4ffcae2a007ae64b43b2edc3e241059b0e43b6
salohiddin1221 <salohiddinkhayitov0@gmail.com>
2022-02-26 11:47:23 +0500

    responsive page

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {FaThList, FaUserGraduate, FaUserAlt} from "react-icons/fa"
+import React, {useState} from 'react'; 
+import {FaThList, FaUserGraduate, FaUserAlt, FaBars} from "react-icons/fa"
 import {RiStackFill} from "react-icons/ri";
 import {HiUserGroup} from "react-icons/hi";
 import {MdCategory} from "react-icons/md";
@@ -7,33 +7,46 @@ import "../sidebar/sidebar.scss";
 import {NavLink} from "react-router-dom";
 
 const Sidebar = () => {
+
+    const [isOpen, setIsOpen] = useState(true);
+
+    const toggleSidebar = () => {
+        setIsOpen(!isOpen);
+    }
+    const toggleTrue = () => {
+        setIsOpen(true);
+    }
+
     return (
-        <div className="sidebar">
+        <div className={isOpen ? "sidebar active" : "sidebar"}>
+            <div className="bars">
+            <FaBars onClick={toggleSidebar} className='bars-icon' />
+            </div>
             <div className="sidebar-wrapper">
                 <div className="sidebar-menu">
-                    <ul className="sidebar-list">
+                    <ul className="sidebar-list" onClick={toggleTrue}>
                         <li className="sidebar-list-item">
                             <NavLink exact={true} to="/category" className="sidebar-list-item_link">
                                 <MdCategory className="sidebar-list-item_icon" />
-                                Bo'limlar
+                               <p className="sidebar-list-item_text"> Bo'limlar</p>
                             </NavLink>
                         </li>
                         <li className="sidebar-list-item">
                             <NavLink to="/courses" className="sidebar-list-item_link">
                                 <FaThList className="sidebar-list-item_icon" />
-                                Kurslar
+                               <p className="sidebar-list-item_text"> Kurslar</p>
                             </NavLink>
                         </li>
                         <li className="sidebar-list-item">
                             <NavLink to="/news" className="sidebar-list-item_link">
                                 <RiStackFill className="sidebar-list-item_icon" />
-                                Yangiliklar
+                                 <p className="sidebar-list-item_text">Yangiliklar</p>
                             </NavLink>
                         </li>
                         <li className="sidebar-list-item">
                             <NavLink to="/teachers" className="sidebar-list-item_link">
                                 <FaUserGraduate className="sidebar-list-item_icon" />
-                                Ustozlar
+                               <p className="sidebar-list-item_text"> Ustozlar </p>
                             </NavLink>
                         </li>
                         <li className="sidebar-list-item">
